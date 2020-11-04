0f77a5d67f9b5cdcc39adba5df999812a604e37e
donaxellema <donaxellema@gmail.com>
2020-11-04 07:27:55 -0500

    Fin seccion 3 - diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -24,16 +25,18 @@ const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
 
-  //{path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  // {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+
   {path: '**', component: NopagefoundComponent},
 ];
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot( routes ) /*para tutas principales */
+  imports: [RouterModule.forRoot( routes ) /*para rutas principales */
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
